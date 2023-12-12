676b512e654c5ab2b32a4a9d4bfcb7367f16237e
Mike Alves <michelalvs@gmail.com>
2023-12-11 21:58:25 -0300

    Created Position Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Helou Uord!");
+		Position position = new Position (3, 5);
+		
+		System.out.println(position.toString());
 	}
 }
