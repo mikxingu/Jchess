5d898c939846486887b704b14aed8115dfd6995e
Mike Alves <michelalvs@gmail.com>
2023-12-12 22:06:38 -0300

    Completed Lecture 183 - Created Board and Piece Classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,10 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Position position = new Position (3, 5);
-		
-		System.out.println(position.toString());
+		Board board = new Board(8,8);
 	}
 }
