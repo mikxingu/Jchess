4a107242b0e7fc2f4cf06ac403d96734001895b7
Mike Alves <michelalvs@gmail.com>
2023-12-18 22:21:32 -0300

    Completed Lecture 188 - Terminal Color Printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,8 +31,9 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('b', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
 	}
 }
