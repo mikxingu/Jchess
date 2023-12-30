1c3cbde2825862bedca79338567a0627ab70022b
Mike Alves <michelalvs@gmail.com>
2023-12-30 16:49:27 -0300

    Completed Lecture 211 - Fix Promotion Special Move

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,51 +13,54 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		ChessMatch match = new ChessMatch();
 		List<ChessPiece> capturedPieces = new ArrayList<>();
-		
-		while(!match.getCheckMate()) {
+
+		while (!match.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(match, capturedPieces);
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = match.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(match.getPieces(), possibleMoves);
-				
+
 				System.out.println();
 				System.out.println("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
+
 				ChessPiece capturedPiece = match.performChessMove(source, target);
-				
+
 				if (capturedPiece != null) {
 					capturedPieces.add(capturedPiece);
 				}
-				
+
 				if (match.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.nextLine();
-					
+					String type = sc.nextLine().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid Value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.nextLine().toUpperCase();
+					}
 					match.replacePromotedChessPiece(type);
 				}
-				
-			} catch(ChessException e) {
+
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			} catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}	
+			}
 		}
-		
+
 		UI.clearScreen();
-		
+
 		UI.printMatch(match, capturedPieces);
 	}
 }
